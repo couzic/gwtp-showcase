7e3054b0320849ba4cb3b43d864c80299af8c6df
micouz <mikaelcouzic@gmail.com>
2014-03-07 17:11:20 +0100

    Programmatic Local change

--- src/com/github/micouz/gwtp/showcase/client/ui/main/HeaderView.java
+++ src/com/github/micouz/gwtp/showcase/client/ui/main/HeaderView.java
@@ -3,9 +3,12 @@ package com.github.micouz.gwtp.showcase.client.ui.main;
 import com.github.gwtbootstrap.client.ui.Brand;
 import com.github.gwtbootstrap.client.ui.Form.SubmitEvent;
 import com.github.micouz.gwtp.showcase.client.handlers.HeaderHandlers;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.http.client.UrlBuilder;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
@@ -38,6 +41,13 @@ public class HeaderView extends ViewWithUiHandlers<HeaderHandlers> implements He
 	@UiHandler("searchBox")
 	void onSubmit(SubmitEvent event) {
 		getUiHandlers().onSearchSubmit();
+	}
+	
+	@UiHandler("french")
+	void onFrenchLinkClick(ClickEvent event) {
+		UrlBuilder url = Window.Location.createUrlBuilder().setParameter(
+				"locale", "fr");
+		Window.Location.assign(url.buildString());
 	}
 
 }
